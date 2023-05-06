d44eaa925d40680cf71cdc91dd8727606188467c
Danil Moskvin <d.moskvin@skbkontur.ru>
2023-05-06 21:57:27 +0500

    add limits

--- Scenes/world.gd
+++ Scenes/world.gd
@@ -1,7 +1,7 @@
 extends Node2D
 @onready var player_camera = $Player/CharacterBody2D/Camera2D
 @onready var sand = $Sand
-
+@onready var player = $Player/CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +11,14 @@ func _ready():
 		player_camera.limit_left = sand_rect.position.x
 		player_camera.limit_right = sand_rect.end.x
 		player_camera.limit_bottom = sand_rect.end.y
+		
+	if player != null:
+		var sand_rect = sand.get_rect()
+		player.min_x = sand_rect.position.x
+		player.max_x = sand_rect.end.x
+		player.min_y = sand_rect.position.y
+		player.max_y = sand_rect.end.y
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scripts/CharacterBody2D.gd
+++ Scripts/CharacterBody2D.gd
@@ -3,11 +3,17 @@ extends CharacterBody2D
 
 const SPEED = 50.0
 const JUMP_VELOCITY = -400.0
-
+var min_x = 0
+var max_x = 0
+var min_y = 0
+var max_y = 0
+# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var coins = 0
 enum State { MOVE, IDLE }
 @export var state = State.IDLE
+
+@onready var shape = $CollisionShape2D
 
 
 func _physics_process(delta):
@@ -27,5 +33,8 @@ func _physics_process(delta):
 		
 	if velocity.is_zero_approx():
 		state = State.IDLE
+	
+	position.x = clamp(position.x, min_x, max_x - shape.position.x * 2)
+	position.y = clamp(position.y, min_y, max_y - shape.position.y * 2)
 
 	move_and_slide()
